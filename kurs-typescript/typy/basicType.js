346b8dea4732a3e54a70b796bcae6c1ee8792433
Rafal Woronowicz <woronowiczJAVA@gmail.com>
2019-07-19 23:57:40 +0200

    angular basics

---
+++ kurs-typescript/typy/basicType.js
@@ -0,0 +1,11 @@
+var BasicType = /** @class */ (function () {
+    function BasicType() {
+        this.name = 'Reksio';
+        this.age = 99;
+        this.colors = ['czarny', 'bialy'];
+        this.dates = [new Date(), new Date()];
+        this.pair = ['Poniedzialek', 22];
+        this.xyz = 222;
+    }
+    return BasicType;
+}());
---
+++ kurs-typescript/typy/basicType.ts
@@ -0,0 +1,9 @@
+class BasicType {
+
+    name: string = 'Reksio';
+    age: number = 99;
+    colors: string[] = ['czarny', 'bialy'];
+    dates: Array<Date> = [new Date(), new Date()];
+    pair: [string, number] = ['Poniedzialek', 22];
+    xyz: any = 222;
+}
